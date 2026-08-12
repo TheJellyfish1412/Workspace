98ca90bc933ac8f871bc577e4169007ee33be3ba
TheJellyfish1412 <152600168+TheJellyfish1412@users.noreply.github.com>
2026-08-13 02:19:26 +0700

    Add files via upload

--- games/Final_Swam/main.lua
+++ games/Final_Swam/main.lua
@@ -847,4 +847,13 @@ Setting_2:TextBox("webhook", "url webhook discord", function(txt)
     end
 end)
 
+Setting_2:Toggle("Render", getgenv().RFManager["Render"], true, function(toggle)
+    if getgenv().RFManager["Render"] ~= toggle then
+        getgenv().RFManager["Render"] = toggle
+        func_RFM:Store()
+    end
+
+    game:GetService("RunService"):Set3dRenderingEnabled(toggle)
+end)
+
 getgenv().Loaded = true
